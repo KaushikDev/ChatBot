04c96ecfa01ea8dc0f422969006d528a3a705420
Piyush Kaushik <piyush.kaushik02@gmail.com>
2017-07-06 17:34:25 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -8,3 +8,47 @@
     messagingSenderId: "815208598749"
   };
   firebase.initializeApp(config);
+
+
+$("document").ready(function(){
+
+const signinGoogle = document.getElementById("googleAuth");
+const signout = document.getElementById("signout");
+ 
+ 
+ 	if(signinGoogle){
+		 googleAuth.addEventListener('click', e=>{
+			firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(function(result) {	 
+			// This gives you a Google Access Token. You can use it to access the Google API.
+  			var tokenGoogle = result.credential.accessToken;
+			  // The signed-in user info.
+			  var userGoogle = result.user;
+			  // ...Below line to be rmeooved if not working expectedly.
+				// var user = firebase.auth().currentUser;
+			}).catch(function(error) {
+ 			 // Handle Errors here.
+ 			 var errorCode = error.code;
+			  var errorMessage = error.message;
+			  // The email of the user's account used.
+			  var email = error.email;
+ 			 // The firebase.auth.AuthCredential type that was used.
+  			var credential = error.credential;
+  			// ...
+			});
+		 });
+		
+		}
+ 
+ 		if(signout){
+             signout.addEventListener('click', e=>{
+		  
+			 promise = firebase.auth().signOut().then(function(){
+				if(confirm("Do you wish to leave?")){
+				 window.location.href = "/FirebaseTODOapp/index.html";
+			 }	
+					 });
+		      promise.catch(e => 
+	                console.log(e.message))
+			});
+		 }
+});
