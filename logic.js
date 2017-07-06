d6ec9b516b20bbc0cfb172dfa1ba508e28d8c38c
Piyush Kaushik <piyush.kaushik02@gmail.com>
2017-07-06 17:30:46 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,1 +1,10 @@
 
+ var config = {
+    apiKey: "AIzaSyC60GkqCP5HaPh151Eo4VIMadpYu6e7LV0",
+    authDomain: "chatthemup.firebaseapp.com",
+    databaseURL: "https://chatthemup.firebaseio.com",
+    projectId: "chatthemup",
+    storageBucket: "chatthemup.appspot.com",
+    messagingSenderId: "815208598749"
+  };
+  firebase.initializeApp(config);
